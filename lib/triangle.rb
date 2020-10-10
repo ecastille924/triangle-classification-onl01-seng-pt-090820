dc46f911b4f6a2007197d457b01ec03aa7ab097e
Erick Castille <ecastille924@gmail.com>
2020-10-10 23:28:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,10 @@ class Triangle
   
   def initialize (s1, s2, s3)
     
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 = (s1 + s2)
+      raise TriangleError
+    end
+    
     if s1 <= 0 || s2 <= 0 ||s3 <= 0
       raise TriangleError
     end
@@ -10,9 +14,7 @@ class Triangle
       raise TriangleError
     end
     
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 = (s1 + s2)
-      raise TriangleError
-    end
+    
     
     @s1 = s1
     @s2 = s2
